8f45447daab094dac915f611e7bb2901f381480b
m516 <m516@m516>
2015-11-21 00:33:46 +0800

    11/20 第9章完

--- language/wiki/views.py
+++ language/wiki/views.py
@@ -25,6 +25,7 @@ def category(request, categoryName):
         pass
     return render(request, 'wiki/category.html', context)
 
+
 def addCategory(request):
     template = 'wiki/addCategory.html'
     if request.method=='GET':
@@ -36,6 +37,7 @@ def addCategory(request):
     form.save()
     return redirect(reverse('wiki:wiki'))
     # Or try this: return wiki(request)
+
 
 def addPage(request, categoryName):
     template = 'wiki/addPage.html'
@@ -55,4 +57,41 @@ def addPage(request, categoryName):
     page = form.save(commit=False)
     page.category = pageCategory
     page.save()
-    return redirect(reverse('wiki:category', args=(categoryName, ))) 
+    return redirect(reverse('wiki:category', args=(categoryName, )))
+
+def deleteCategory(request, categoryID):
+    if request.method!='POST':
+        return wiki(request)
+    # request.method=='POST':
+    categoryToDelete = Category.objects.get(id=categoryID)
+    if categoryToDelete:
+        categoryToDelete.delete()
+    return redirect(reverse('wiki:wiki'))
+
+def deletePage(request, pageID):
+    if request.method!='POST':
+        return wiki(request)
+    # request.method=='POST':
+    pageToDelete = Page.objects.get(id=pageID)
+    if pageToDelete:
+        categoryName = pageToDelete.category.name
+        pageToDelete.delete()
+    else:
+        categoryName = ''
+    return redirect(reverse('wiki:category', args=(categoryName, )))
+
+def updateCategory(request, categoryID):
+    template = 'wiki/updateCategory.html'
+    try:
+        categoryToUpdate = Category.objects.get(id=categoryID)
+    except Category.DoesNotExist:
+        return wiki(request)
+    if request.method=='GET':
+        form = CategoryForm(instance=categoryToUpdate)
+        return render(request, template, {'form':form, 'category':categoryToUpdate})
+    # request.method=='POST'
+    form = CategoryForm(request.POST, instance=categoryToUpdate)
+    if not form.is_valid():
+        return render(request, template, {'form':form, 'category':categoryToUpdate})
+    categoryToUpdate.save()
+    return redirect(reverse('wiki:wiki'))
